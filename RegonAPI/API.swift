5095c57606bac41c40f248e2a3763dfa7c122ce7
Guest User <Guest@Mac-mini-8.local>
2022-11-29 12:58:29 +0100

    progress

--- RegonAPI/API.swift
+++ RegonAPI/API.swift
@@ -1,54 +1,88 @@
 
 import Alamofire
 import RxSwift
+import Foundation
 
 
 struct API {
- /*   func getApi() -> Observable<AnyObject?> {
-        return Observable.create{ observer in
-            let request = AF.request(.get, "http://someapiurl.com", parameters: nil)
-                    .response(completionHandler:  { request, response, data, error in
-                        if ((error) != nil) {
-                            observer.on(.Error(error!))
-                        } else {
-                            observer.on(.Next(data))
-                            observer.on(.Completed)
-                        }
-                    });
-            return Disposable {
-                request.cancel()
-            }
-        }
-    }*/
-    private static var URL = "https://rejestr.io/api/v2/org"
+    /*   func getApi() -> Observable<AnyObject?> {
+     return Observable.create{ observer in
+     let request = AF.request(.get, "http://someapiurl.com", parameters: nil)
+     .response(completionHandler:  { request, response, data, error in
+     if ((error) != nil) {
+     observer.on(.Error(error!))
+     } else {
+     observer.on(.Next(data))
+     observer.on(.Completed)
+     }
+     });
+     return Disposable {
+     request.cancel()
+     }
+     }
+     }*/
+    private static var endpoint = "https://rejestr.io/api/v2/org"
     func getCompanies(search:String) -> Observable<[Company]> {
         if(search.isEmpty) {
             return Observable.just([])
         }
-        return Observable.create{ observer in
-            let request = AF.request(API.URL + "?nazwa=" + search, method:.get)
-            request.responseJSON{
-                (response) in
-                if let status = response.response?.statusCode {
-                    switch(status){
-                        case 201:
-                            print("example success")
-                        default:
-                            print("error with response status: \(status)")
-                                    }
-                                }
-                //to get JSON return value
-                            if let result = response.result.value {
-                                let JSON = result as! NSDictionary
-                                print(JSON)
+        let url = try! URLRequest(url: URL(string:API.endpoint + "?nazwa=" + search)!, method: .get, headers: headers())
+        
+        return Observable.create { observer in
+            let request = AF.request(url)
+                .validate()
+                .responseJSON { response in
+                    //print(response)
+                    switch response.result {
+                    case .success(let jsonObj):
+                        
+                        //guard let json = try? JSON(data:response.result.value)else{fallthrough}
+                       //   let json = try? JSON(data:response.result.value) else {return}
+                        let json = jsonObj as! NSDictionary
+                        var companies :[Company] = []
+                        
+                        
+                        if let results = json["wyniki"] as? [[String:Any]] {
+                            for result in results {
+                                let name = (result["nazwy"] as? [[String:Any]])
+                                let nameFull = name["pelna"]
+                                let taxIdNo = result["numery"]["nip"]
+                                let registrationDate = result["krs_wpisy"]["pierwszy_data"]
+                                let address = result["krs_adresy"]
+                                let ceo = result["glowna_osoba"]["imiona_i_nazwisko"]
+                                
+                                companies.append(Company(
+                                    name:"",
+                                    taxIdNo:"",
+                                    registrationDate: "",
+                                    addressString: "",
+                                    addressLat: 0,
+                                    addressLong: 0,
+                                    ceo:""
+                                ))
                             }
-            }
-            let companies = [Company.sampleData[0]]
-            
-            observer.onNext(companies)
-            return Disposables.create{
+                        }
+                    
+                        observer.on(.next(companies))
+                    case .failure(let error):
+                        print("error")
+                        observer.on(.error(error))
+                    }
+                }
+            return Disposables.create {
                 request.cancel()
             }
         }
     }
-}
+    
+    func headers() -> HTTPHeaders {
+        var headers: HTTPHeaders = [
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        ]
+        
+        headers["Authorization"] = "17dad445-657b-47c8-90dd-5b60f366f8d4"
+        
+        
+        return headers
+    }}
--- RegonAPI/Company.swift
+++ RegonAPI/Company.swift
@@ -5,17 +5,21 @@ struct Company: Identifiable{
     var id: UUID
     var name: String
     var taxIdNo: String
+    var addressString:String
     var addressLat: Double
     var addressLong: Double
     var registrationDate: String
+    var ceo:String
 
-    init(id: UUID = UUID(), name: String,  taxIdNo:String,registrationDate:String,addressLat:Double,addressLong:Double) {
+    init(id: UUID = UUID(), name: String,  taxIdNo:String,registrationDate:String,addressString:String,addressLat:Double,addressLong:Double,ceo:String) {
         self.id = id
         self.name = name
         self.registrationDate = registrationDate
         self.taxIdNo = taxIdNo
+        self.addressString = addressString
         self.addressLat = addressLat
         self.addressLong = addressLong
+        self.ceo = ceo
     }
 
 }
@@ -36,8 +40,8 @@ extension Company{
 extension Company {
     static let sampleData: [Company] =
             [
-                Company(name: "Orlen", taxIdNo: "7740001454", registrationDate: "2001-07-19", addressLat:50,addressLong:0),
-                Company(name: "Grupa Lotos",  taxIdNo:"5830000960", registrationDate: "2002-04-10", addressLat:30, addressLong:13),
-                Company(name: "Energa", taxIdNo:"5252453436", registrationDate: "20017-01-08", addressLat:13,addressLong:50)
+                Company(name: "Orlen", taxIdNo: "7740001454", registrationDate: "2001-07-19",addressString: "" ,addressLat:50,addressLong:0,ceo:"Daniel"),
+                Company(name: "Grupa Lotos",  taxIdNo:"5830000960", registrationDate: "2002-04-10", addressString: "" ,addressLat:30, addressLong:13,ceo:"Jan"),
+                Company(name: "Energa", taxIdNo:"5252453436", registrationDate: "20017-01-08",addressString: "" , addressLat:13,addressLong:50,ceo:"Bartosz")
             ]
 }
